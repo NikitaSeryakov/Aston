142c570929aa0855c75cd613002ed7a3cf65fa9e
Nikita.Seryakov <Seryakov345256@mail.ru>
2026-04-21 22:27:21 +0600

    add new class

---
+++ src/main/java/Product.java
@@ -0,0 +1,31 @@
+public class Product {
+    private String name;
+    private String data;
+    private String manufacturer;
+    private String country;
+    private double price;
+    private boolean isReserved;
+
+    private Product(String name, String data, String manufacturer, String country, double price, boolean isReserved) {
+        this.name = name;
+        this.data = data;
+        this.manufacturer = manufacturer;
+        this.country = country;
+        this.price = price;
+        this.isReserved = isReserved;
+    }
+
+    public void info() {
+        String status = isReserved ? "Забронирован" : "Не забронирован";
+        System.out.println("Товар: " + name);
+        System.out.println("Дата производства: " + data);
+        System.out.println("Производитель: " + manufacturer + " (" + country + ")");
+        System.out.println("Цена: " + price);
+        System.out.println("Статус: " + status);
+    }
+
+    public static void main(String[] args) {
+        Product item = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 85000.0, true);
+        item.info();
+    }
+}
---
+++ src/main/java/arraysProduct.java
@@ -0,0 +1,10 @@
+public class arraysProduct {
+    static void Product() {
+        Product[] arraysProduct = new Product[5];
+        arraysProduct[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        arraysProduct[1] = new Product("iPhone 16 Pro", "20.09.2024", "Apple Inc.", "USA", 4999, true);
+        arraysProduct[2] = new Product("Sony WH-1000XM5", "12.05.2023", "Sony", "Japan", 1200, true);
+        arraysProduct[3] = new Product("MacBook Air M3", "08.03.2024", "Apple Inc.", "USA", 7500, true);
+        arraysProduct[4] = new Product("Xiaomi 14 Ultra", "22.02.2024", "Xiaomi", "China", 3800, false);
+    }
+}
