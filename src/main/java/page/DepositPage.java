e979e4b44227423982d563507a6d6adf0066a5b9
Nikita.Seryakov <Seryakov345256@mail.ru>
2026-05-17 20:18:51 +0600

    add new classes

---
+++ src/main/java/page/DepositPage.java
@@ -0,0 +1,5 @@
+package page;
+
+public class DepositPage extends BasePage {
+
+}
--- src/main/java/page/MainPage.java
+++ src/main/java/page/MainPage.java
@@ -1,7 +1,17 @@
 package page;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MainPage extends BasePage {
+    @FindBy(xpath = "//*[text() = 'Принять']")
+    private WebElement cookieButton;
+    public MainPage openPage(String url) {
+        driver.get(url);
+        return this;
+    }
+    public MainPage acceptCookie(){
+        cookieButton.click();
+        return this;
+    }
 }
